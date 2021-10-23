50139e838cd75e33afa97d53a10146e5f8ab4766
koseali <kose.ali@std.izu.edu.tr>
2021-10-23 22:54:32 +0300

    add  view extension inspectable corner radius

--- TodoApp/TodoApp/Extensions/UIView+.swift
+++ TodoApp/TodoApp/Extensions/UIView+.swift
@@ -10,4 +10,10 @@ import UIKit
 
 extension UIView {
     
+    @IBInspectable
+    var cornerRadius : CGFloat{
+        get { return self.cornerRadius}
+        set { self.layer.cornerRadius = newValue}
+    }
+    
 }
