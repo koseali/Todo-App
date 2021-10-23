12e35d3dddb3001ceb30d0780c811a6bb93fe5b9
koseali <kose.ali@std.izu.edu.tr>
2021-10-23 21:57:31 +0300

    add tableview custom cell

---
+++ TodoApp/TodoApp/Main/MainViewTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  MainViewTableViewCell.swift
+//  TodoApp
+//
+//  Created by 503512 on 20.10.2021.
+//
+
+import UIKit
+
+class MainViewTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var detailLabel: UILabel!
+    @IBOutlet weak var deadlineDateLabel: UILabel!
+}
