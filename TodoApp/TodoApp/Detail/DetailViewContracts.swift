024ad57dfe0c3bbdfb2d12d00217b26af6db7fbf
koseali <kose.ali@std.izu.edu.tr>
2021-10-23 22:17:42 +0300

    add detailview mvvm protocols

--- TodoApp/TodoApp/Detail/DetailViewContracts.swift
+++ TodoApp/TodoApp/Detail/DetailViewContracts.swift
@@ -1,8 +1,39 @@
 //
-//  File.swift
+//  DetailContracts.swift
 //  TodoApp
 //
-//  Created by Ali Kose on 23.10.2021.
+//  Created by Ali Kose on 20.10.2021.
 //
 
 import Foundation
+
+// MARK: VIEWMODEL
+
+protocol DetailViewModelProtocol : AnyObject {
+    var delegate : DetailViewModelDelegate? { get set }
+    func didLoad()
+    func addTodo(title: String , detail : String , deadlineDate : String, date : Date)
+    func goBack()
+}
+protocol DetailViewModelDelegate : AnyObject {
+    func handleOutput(_ output : ViewModelOutput)
+    func navigate(to route : DetailViewRouter)
+}
+
+enum DetailViewRouter {
+    case goBack
+    case saveAndBack
+}
+
+enum ViewModelOutput {
+    case showToDoDetail(ToDoPresentation)
+    case showToDoAdd
+    case showAlert
+    
+}
+// MARK: BUILDER SCREEN TYPE
+
+enum builderChoice {
+    case DetailView
+    case AddView
+}
