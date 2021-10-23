3794bd87e45e600aee76a473d42501440bee6b45
koseali <kose.ali@std.izu.edu.tr>
2021-10-23 22:18:28 +0300

    add detail view builder class

--- TodoApp/TodoApp/Detail/DetailViewBuilder.swift
+++ TodoApp/TodoApp/Detail/DetailViewBuilder.swift
@@ -2,7 +2,23 @@
 //  DetailViewBuilder.swift
 //  TodoApp
 //
-//  Created by Ali Kose on 23.10.2021.
+//  Created by Ali Kose on 20.10.2021.
 //
 
 import Foundation
+
+class DetailViewBuilder {
+    static func build(with todo : TodoTasks, screenType : builderChoice) -> DetailViewController {
+        let view = DetailViewController()
+        let viewModel = DetailViewModel(todoItem: todo, dataManager: appContainer.dataManager, notificationManager: LocalNotificationManager())
+        viewModel.screenType = screenType
+        view.viewModel = viewModel
+        return view
+    }
+    
+    deinit {
+        print("DetailViewBuilder deinit oldu.")
+    }
+    
+   
+}
