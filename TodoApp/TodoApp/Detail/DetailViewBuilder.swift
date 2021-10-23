40c79d7a4a2e7035137f8f2d332d8e10bb0e674f
koseali <kose.ali@std.izu.edu.tr>
2021-10-23 22:15:30 +0300

    add detailscreen mvvm design pattern

---
+++ TodoApp/TodoApp/Detail/DetailViewBuilder.swift
@@ -0,0 +1,8 @@
+//
+//  DetailViewBuilder.swift
+//  TodoApp
+//
+//  Created by Ali Kose on 23.10.2021.
+//
+
+import Foundation
---
+++ TodoApp/TodoApp/Detail/DetailViewContracts.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  TodoApp
+//
+//  Created by Ali Kose on 23.10.2021.
+//
+
+import Foundation
