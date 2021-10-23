69f1bc9a55df5761125b6f7a5b373f6caa75cc4d
koseali <kose.ali@std.izu.edu.tr>
2021-10-23 21:22:21 +0300

    Add xcode project

---
+++ TodoApp/TodoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodoApp
+//
+//  Created by Ali Kose on 23.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
