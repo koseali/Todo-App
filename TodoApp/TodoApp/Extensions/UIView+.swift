5eb5b60db1ef92a11bc339464165613f97b0a2be
koseali <kose.ali@std.izu.edu.tr>
2021-10-23 22:56:29 +0300

    update changeKeyboard function

--- TodoApp/TodoApp/Extensions/UIView+.swift
+++ TodoApp/TodoApp/Extensions/UIView+.swift
@@ -22,6 +22,15 @@ extension UIView {
     
     @objc func  changeKeyboard(_ notification : NSNotification){
         
+        let time = notification.userInfo![UIResponder.keyboardAnimationDurationUserInfoKey] as! Double
+        
+        let curve = notification.userInfo![UIResponder.keyboardAnimationCurveUserInfoKey] as! UInt
+        
+        let startPosition = (notification.userInfo![UIResponder.keyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
+        let endPosition = (notification.userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        let different =  endPosition.origin.y - startPosition.origin.y
+        UIView.animateKeyframes(withDuration: time, delay: 0.0, options: KeyframeAnimationOptions.init(rawValue: curve), animations: {self.frame.origin.y += different}, completion: nil)
+        
     }
     
 }
