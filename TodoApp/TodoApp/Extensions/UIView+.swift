4fccf255105f3c5e20ef49b7a9fc51561fa04a0f
koseali <kose.ali@std.izu.edu.tr>
2021-10-23 22:52:09 +0300

    add extensions files

---
+++ TodoApp/TodoApp/Extensions/UIView+.swift
@@ -0,0 +1,13 @@
+//
+//  UIView+.swift
+//  TodoApp
+//
+//  Created by Ali Kose on 23.10.2021.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    
+}
---
+++ TodoApp/TodoApp/Extensions/UIViewController+.swift
@@ -0,0 +1,13 @@
+//
+//  UIViewController+.swift
+//  TodoApp
+//
+//  Created by Ali Kose on 23.10.2021.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+    
+}
