1171ac4842ef5ae30de1ad0d1ca491cdf3734027
koseali <kose.ali@std.izu.edu.tr>
2021-10-23 21:54:15 +0300

    add router layer

---
+++ TodoApp/TodoApp/Main/MainViewRouter.swift
@@ -0,0 +1,32 @@
+//
+//  MainViewRouter.swift
+//  TodoApp
+//
+//  Created by 503512 on 20.10.2021.
+//
+
+import Foundation
+import UIKit
+
+class MainvViewRouter : MainViewRouterProtocol {
+    var view : UIViewController
+    init(view : UIViewController){
+        self.view = view
+    }
+    
+    func navigate(to route: MainViewRoute) {
+        switch route {
+        case .showBookDetail(let todoTasks):
+            let detailView = DetailViewBuilder.build(with: todoTasks,
+                                                     screenType: .DetailView)
+            view.navigationController?.pushViewController(detailView, animated: true)
+            
+        case .showAddView:
+            let addView = DetailViewBuilder.build(with: TodoTasks(), screenType: .AddView)
+            view.navigationController?.pushViewController(addView, animated: true)
+        }
+    }
+    deinit {
+        print("MainViewRouter deinit oldu.")
+    }
+}
